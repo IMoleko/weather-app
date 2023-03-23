7e1bfdddd3db62b07107cd26971550cefc023747
Ivan <113191113+IMoleko@users.noreply.github.com>
2023-03-23 13:34:37 +0000

    More stuff

--- Gui/Gui.py
+++ Gui/Gui.py
@@ -1,1 +1,52 @@
-here
+import tkinter
+from tkinter import *
+from tkinter messagebox
+
+
+
+def createGUI():
+    form.tk()
+    form.title("Welcome to the weather app!")
+    form.geography(500X200)
+    welcomeLabel = Label(form, text="Welcome to the weather app")
+    welcomeLabel.config(font=("Courier", = 14))
+    welcomeLabel.grid(row=0, column=0, columnspan=3, sticky="W", padx=10, pady=10)
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
